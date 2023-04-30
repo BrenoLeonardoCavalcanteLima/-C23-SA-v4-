cbeb1e59621dcfa0f18846d436377600378484c1
BrenoLeonardoCavalcanteLima <paulo.victor.cavalcante.costa>
2023-04-30 13:42:36 -0300

    C23

--- sketch.js
+++ sketch.js
@@ -1,32 +1,37 @@
 
+const Engine=Matter.Engine
+const World =Matter.World
+const Bodies =Matter.Bodies
+const Body = Matter.Body
+let world,engine
+  
+var ball
 
-
-//Crie um namespace (espaço de nomes) para Mecanismo
-//Crie um namespace (espaço de nomes) para Mundo
-//Crie um namespace (espaço de nomes) para Corpos
-//Crie um namespace (espaço de nomes) para Corpo
-
-
+var ground
 function setup() {
   createCanvas(400,400);
-//crie o mecanismo
-  //Adicione mundo ao mecanismo
-  
 
   
-   var ball_options = {
+engine=Engine.create()
+world=engine.world
+
+  var ball_options = {
     restitution: 0.95,
     frictionAir:0.01
   }
    
+
+
    var ground_options ={
      isStatic: true
    };
   
   
 //crie o solo
-//adicione ao mundo
+ground=Bodies.rectangle(200,400,400,20,{isStatic:true})
+World.add(world,ground);
 
+//adicione ao mundo
   ball = Bodies.circle(100,10,20,ball_options);
   World.add(world,ball);
   
@@ -44,7 +49,7 @@ function draw()
   Engine.update(engine);
   
   
-
+  rect(ground.position.x,ground.position.y,400,20)
   ellipse(ball.position.x,ball.position.y,20);
   //escreva uma função de retângulo para exibir o solo.
  
